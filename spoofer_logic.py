e2ac6f2a492c0d2ea74826de84e8a2009664ba6c
ArmykCZ <Armycek@proton.me>
2026-04-18 23:38:26 +0200

    vytvoření prvotní logiky programu

--- spoofer_logic.py
+++ spoofer_logic.py
@@ -12,3 +12,20 @@
 
 # 4. TODO: Funkce save_capture(packet_list, filename="capture.pcap")
 #    - Použít wrpcap() k uložení nasbíraných dat.
+from scapy.all import ARP, send, wrpcap, sr1, sr
+
+def spoof():
+
+    ip_obet = input("Zadejte IP adresu oběti: ")
+    ip_router = input("Zadejte IP adresu routeru: ")
+
+    mac_obet = sr1(ARP(pdst=ip_obet), timeout=2, verbose=False).hwsrc
+    mac_router = sr1(ARP(pdst=ip_router), timeout=2, verbose=False).hwsrc
+
+    if mac_obet is None:
+        print(f"Nepodařilo se získat MAC adresu oběti ({ip_obet}).")
+    
+    if mac_router is None:
+        print(f"Nepodařilo se získat MAC adresu routeru ({ip_router}).")
+
+    
